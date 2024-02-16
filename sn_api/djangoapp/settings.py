7b5071a21937b50471487be8b0be70897f2d07ec
Rafael R Rosa <rarorza@proton.me>
2024-02-16 16:47:36 -0300

    feat: makes possible to search for posts and users

--- sn_api/djangoapp/settings.py
+++ sn_api/djangoapp/settings.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = [
     # apps
     "account",
     "post",
+    "search",
 ]
 
 MIDDLEWARE = [
--- sn_api/djangoapp/urls.py
+++ sn_api/djangoapp/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path("api/", include("account.urls")),
     path("api/posts/", include("post.urls")),
+    path("api/search/", include("search.urls")),
     path("admin/", admin.site.urls),
 ]
