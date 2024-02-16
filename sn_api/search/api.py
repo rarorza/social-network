70fe5b70a34132e68971d13330ed433fbd917020
Rafael R Rosa <rarorza@proton.me>
2024-02-16 17:35:41 -0300

    refactor: convert feed card to a separate component

--- sn_api/search/api.py
+++ sn_api/search/api.py
@@ -15,12 +15,14 @@ def search(request):
     data = request.data
     query = data["query"]
 
-    users = User.objects.filter(name__icontains=query)
-    users_serializer = UserSerializer(users, many=True)
+    if len(query) >= 2:
+        users = User.objects.filter(name__icontains=query)
+        users_serializer = UserSerializer(users, many=True)
 
-    posts = Post.objects.filter(body__icontains=query)
-    posts_serializer = PostSerializer(posts, many=True)
-    return JsonResponse(
-        {"users": users_serializer.data, "posts": posts_serializer.data},
-        safe=False,
-    )
+        posts = Post.objects.filter(body__icontains=query)
+        posts_serializer = PostSerializer(posts, many=True)
+        return JsonResponse(
+            {"users": users_serializer.data, "posts": posts_serializer.data},
+            safe=False,
+        )
+    return JsonResponse({"error": "query length < 2"}, status=400)
