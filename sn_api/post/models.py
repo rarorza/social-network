4e21a004552799209bca0c316b1f0e1f1b277bdc
Rafael R Rosa <rarorza@proton.me>
2024-02-16 19:27:39 -0300

    feat: add friendship model

--- sn_api/post/models.py
+++ sn_api/post/models.py
@@ -19,8 +19,10 @@ class Post(models.Model):
     attachments = models.ManyToManyField(PostAttachment, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(
-        User, related_name="posts", on_delete=models.CASCADE
-    )  # noqa 501
+        User,
+        related_name="posts",
+        on_delete=models.CASCADE,
+    )
 
     class Meta:
         ordering = ("-created_at",)
