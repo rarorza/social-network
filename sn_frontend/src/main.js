c752e00bcab521772c6b4eb9c6977e1dfbaa761f
Rafael R Rosa <rarorza@proton.me>
2024-03-18 19:26:53 -0300

    ops: prepares .env files to front end

--- sn_frontend/src/main.js
+++ sn_frontend/src/main.js
@@ -8,6 +8,7 @@ import router from './router'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
 
 const app = createApp(App)
 
