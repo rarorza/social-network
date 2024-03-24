47d1abad814c9bd1b24af1cb93e82e0967348dcd
Rafael R Rosa <rarorza@proton.me>
2024-03-24 16:41:35 -0300

    feat: improves the messages view, showing the user's friends as chat suggestions.

--- sn_frontend/src/views/MessagesView.vue
+++ sn_frontend/src/views/MessagesView.vue
@@ -1,12 +1,13 @@
 <script setup>
-import axios from 'axios';
-import { useUserStore } from '@/stores/user';
-import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import { useUserStore } from '@/stores/user'
+import { ref, onMounted, watch, computed } from 'vue'
 
 const userStore = useUserStore()
 const conversations = ref([])
-const activeConversation = ref({})
+const activeConversation = ref({ messages: null })
 const body = ref('')
+const friends = ref([])
 
 function setActiveConversation(conversation) {
   activeConversation.value = conversation
@@ -24,9 +25,8 @@ function getMessages() {
 function getConversation() {
   axios.get('/api/chat/').then(response => {
     conversations.value = response.data
-    console.log('getConversation', response.data);
 
-    if (conversations.value) {
+    if (!activeConversation.value.messages) {
       activeConversation.value = conversations.value[0]
     }
 
@@ -41,14 +41,54 @@ function submitForm() {
     body: body.value
   }).then(response => {
     activeConversation.value.messages.push(response.data)
+    body.value = ''
   }).catch(error => {
     console.log(error);
   })
 }
 
+function getFriends() {
+  axios.get(`/api/friends/${userStore.user.id}/`).then(response => {
+    friends.value = response.data.friends
+
+    for (const i in conversations.value) {
+      for (const iUser in conversations.value[i].users) {
+        friends.value = friends.value.filter(
+          friend => friend.id !== conversations.value[i].users[iUser].id
+        )
+      }
+    }
+  }).catch(error => {
+    console.log('Error', error);
+  })
+}
+
+function sendMessage(idFriend) {
+  axios.get(`/api/chat/get-or-create/${idFriend}/`).then(response => {
+    friends.value = friends.value.filter(friend => friend.id !== idFriend)
+    getConversation()
+    setActiveConversation(response.data)
+  }).catch(error => {
+    console.log('error', error)
+  })
+}
+
+const friendsLength = computed(() => {
+  return Object.keys(friends.value).length
+})
+
 onMounted(() => {
   getConversation()
+  getFriends()
 })
+
+watch(() => conversations, () => {
+  getConversation()
+}, { immediate: true })
+
+watch(() => friends, () => {
+  getFriends()
+}, { immediate: true })
 </script>
 
 <template>
@@ -63,14 +103,29 @@ onMounted(() => {
             <div class="flex items-center space-x-2">
               <template v-for="user in conversation.users" :key="user.id">
                 <template v-if="user.id !== userStore.user.id">
-                  <img :src="user.get_avatar" class="w-[40px] rounded-full cursor-pointer">
-                  <p class="text-xs font-bold cursor-pointer">{{ user.name }}</p>
+                  <img :src="user.get_avatar"
+                    class="w-[40px] rounded-full cursor-pointer">
+                  <p class="text-xs font-bold cursor-pointer">{{ user.name }}
+                  </p>
                 </template>
               </template>
             </div>
             <span class="text-xs text-gray-500">{{
             conversation.modified_at_formatted }}</span>
           </div>
+
+          <hr v-if="friendsLength">
+          <template v-for="friend in friends" :key="friend.id">
+            <div class="flex items-center justify-between">
+              <div class="flex items-center space-x-2"
+                @click="sendMessage(friend.id)">
+                <img :src="friend.get_avatar"
+                  class="w-[40px] rounded-full cursor-pointer">
+                <p class="text-xs font-bold cursor-pointer">{{ friend.name }}
+                </p>
+              </div>
+            </div>
+          </template>
 
         </div>
       </div>
@@ -79,6 +134,7 @@ onMounted(() => {
     <div class="main-center col-span-3 space-y-4">
       <div class="bg-white border border-gray-200 rounded-lg ">
         <div class="flex flex-col flex-grow p-4">
+
 
           <template v-for="message in activeConversation.messages"
             :key="message.id">
@@ -113,8 +169,6 @@ onMounted(() => {
               </div>
             </div>
           </template>
-
-
 
         </div>
       </div>
